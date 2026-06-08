7dfa5b71bd0b8bd2e4cef633955f6d8b4615c798
Divyanshu <divyanshu.4987@gmail.com>
2026-06-08 12:29:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Number Guesser
+
+A simple JavaScript number guessing game.
+
+## Features
+
+* Random number generation
+* User-defined maximum number
+* High/Low hints
+* Guess counter
+* Quit option (`q`)
+
+## How to Run
+
+Open the JavaScript file in a browser environment that supports `prompt()` and `console.log()`.
+
+## Author
+
+Made while learning JavaScript.
---
+++ script.js
@@ -0,0 +1,25 @@
+let maxnum = Number(prompt("Welcome! Enter your max number:"));
+let num = Math.floor(Math.random()*maxnum)+1; // the target will be chosen randomly from 1 to maxnum
+let input = prompt("Guess the number:");
+let i=0;
+while(true){
+    if(input.toLowerCase()==="q"){
+        console.log("Game quit!");
+        break;
+    }
+    else if(input<num){
+        input = prompt("Too low! Guess again");
+        i++;
+    }
+    else if(input>num){
+        input = prompt("Too high! Guess again");
+        i++;
+    }
+    else if(input==num){
+        console.log(`You took ${i+1} guesses`);
+        break;
+    }
+    else{
+        input = prompt("Please enter number only!");
+    }
+}
